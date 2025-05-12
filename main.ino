dd7241f51a6f145e0b9b8344d63eb3bdfcf56120
RedZunder <100701496+RedZunder@users.noreply.github.com>
2025-05-12 11:42:51 +0200

    Update main.ino
    Corrected comments and removed unnecesary code

--- main.ino
+++ main.ino
@@ -1,36 +1,43 @@
 #include <SoftwareSerial.h>
 
-// Try different baud rates here (9600, 38400, 57600, 115200)
 SoftwareSerial BTSerial(10, 11); // RX, TX
+
+//pins
 #define R 3
 #define G 6
 #define B 5
 
-void setup() {
-  Serial.begin(9600);      // Monitor
-  BTSerial.begin(9600);    // Test baud
-  Serial.println("Echo test started. Type below:");
+void setup() 
+{
+    Serial.begin(9600);
+    BTSerial.begin(9600);
+    Serial.println("Echo test started. Type below:");
 }
+
 String x="";
-int i=0;
-void loop() {
-  if (Serial.available()) {
-    x[i] = Serial.read();
-    BTSerial.print(x);
-  }
 
-  if (BTSerial.available()) {
-    x=BTSerial.readString();
-    Serial.print(x);
-    decode(x);
-    i++;
+void loop() 
+{
+
+  if (BTSerial.available()) 
+  {
+      x=BTSerial.readString();
+      Serial.print(x);
+      decode(x);
   }
 
 }
 
+
+
+/**
+ * @brief   Turn on the right colors for the RGB LED
+ *          by parsing char to int and scaling to 0-255.
+ * @ss      The string received from Bluetooth, RxGxBx
+ */
 void decode(String ss)
 {
-    analogWrite(R,(ss[1]-'0') *25);
+    analogWrite(R,(ss[1]-'0') *25);    
     analogWrite(G,(ss[3]-'0') *25);
     analogWrite(B,(ss[5]-'0') *25);
 }
